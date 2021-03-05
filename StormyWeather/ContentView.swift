ce627b44bd5702f81acc3db11e08aea46ff4d9bd
Maxim Kirkman <makirkman@deloitte.com.au>
2021-03-05 12:02:25 +1100

    Initial Commit

---
+++ StormyWeather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StormyWeather
+//
+//  Created by Kirkman, Max on 5/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StormyWeather/StormyWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  StormyWeatherApp.swift
+//  StormyWeather
+//
+//  Created by Kirkman, Max on 5/3/21.
+//
+
+import SwiftUI
+
+@main
+struct StormyWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
